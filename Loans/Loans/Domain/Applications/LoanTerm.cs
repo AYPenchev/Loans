0e33614ddeda14b49e7d237c89f96c9ee98068b7
Asen Penchev <asen.penchev@grafixoft.com>
2019-11-11 09:24:13 -0500

    Initial setup

---
+++ Loans/Loans/Domain/Applications/LoanTerm.cs
@@ -0,0 +1,30 @@
+﻿namespace Loans.Domain.Applications
+{
+    using Domain;
+    using System;
+    using System.Collections.Generic;
+
+    public class LoanTerm : ValueObject
+    {
+        public int Years { get; }
+
+        private LoanTerm() { }
+
+        public LoanTerm(int years)
+        {
+            if (years < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(years), "Please specify a value greater than 0.");
+            }
+
+            Years = years;
+        }
+
+        public int ToMonths() => Years * 12;
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Years;
+        }
+    }
+}
